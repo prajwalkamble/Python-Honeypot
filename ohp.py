67c69a3e3738cdac55c1a7ef082cbfca29bd8289
Prajwal Kamble <65185064+prajwalkamble@users.noreply.github.com>
2024-01-24 15:36:17 +0530

    Update ohp.py

--- ohp.py
+++ ohp.py
@@ -1,4 +1,3 @@
-# Meow Meow niggah.
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -6,4 +5,4 @@ import sys
 from core.load import load_honeypot_engine
 
 if __name__ == "__main__":
-  sys.exit(0 if load_honeypot_engine() is True else 1)
+sys.exit(0 if load_honeypot_engine() is True else 1)
